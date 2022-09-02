6b4627d774961b7af0bc8d7637e9b3ea99415a79
Irfan Zidni <irfanzidniofficial@gmail.com>
2022-09-03 06:01:04 +0700

    update

--- sesi-03/promise.js
+++ sesi-03/promise.js
@@ -1,13 +1,29 @@
-// Di promise ada 3 state
-// pending, fullfiled, rejected
+//using library node fetch
 import fetch from "node-fetch";
 
-fetch("https://fake-tweets-api.herokuapp.com/posts")
-  .then((response) => response.json())
-  .then((tweets) => {
-    let sizeTweet = tweets.length;
-    console.log("panjang data = " + sizeTweet + "\n" + tweets);
-  })
-  .catch((err) => {
-    console.error(error);
-  });
+// implicit return di arrow function
+const response = await fetch("https://fake-tweets-api.herokuapp.com/posts");
+const tweet = await response.json();
+console.log(tweet);
+
+/* ---------------------------- */
+// bungkus kedalam fungsi
+const fetchTweets = async () => {
+  const response = await fetch("https://fake-tweets-api.herokuapp.com/posts");
+  const tweet = await response.json();
+  console.log(tweet);
+};
+
+fetchTweets();
+
+/* ---------------------------- */
+/*
+    fungsi yang ingin langsung di eksekusi atau 
+    istilah kerennya IIFE (Immediately Invoked Function Expression)
+    bisa kita tulis seperti ini.
+*/
+async () => {
+  const response = await fetch("https://fake-tweets-api.herokuapp.com/posts");
+  const tweet = await response.json();
+  console.log(tweet);
+};
